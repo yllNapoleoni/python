bfb8a8e4c1340a2b2cd87bb61bbb0dd63393ba18
Student <yllaimbontaparta@gmail.com>
2025-10-23 16:12:53 +0200

    module 1

---
+++ modul1/index.py
@@ -0,0 +1,32 @@
+# print('hello world')
+
+# temp=123
+#
+# name='ylli'
+#
+# print(temp)
+# print(name)
+#
+# x=5
+# y=123
+#
+# result=x+y
+#
+# print(result)
+#
+# print('the best student in the class is ',name)
+
+nx=['ylli','darisi','shpati','ensari']
+
+print(nx[0])
+
+
+print(nx[1])
+
+nx.append('milk')
+
+nx.pop(3)
+
+nx.remove('darisi')
+
+print(nx)
