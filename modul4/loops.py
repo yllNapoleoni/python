1cc67bcf69198317156a0e8482716ddd05337027
Student <yllaimbontaparta@gmail.com>
2025-11-11 16:04:38 +0100

    bla bla bla bla bla bla

---
+++ modul4/loops.py
@@ -0,0 +1,62 @@
+# sentence = "hello world"
+#
+# for character in sentence:
+#     if character.isalpha():
+#         print(character)
+#
+
+# numbers = [12,45,22,54,12456,2]
+#
+# maximum=numbers[0]
+#
+# for num in numbers:
+#     if num>maximum:
+#         maximum=num
+# print("the maximum number value in the list is:" , maximum)
+
+count=1
+
+# while count<=5:
+#     print("iteration:", count)
+#     count+=1
+
+# numbers=[1,2,3,4,5]
+# target=4
+#
+# for number in numbers:
+#     print(number)
+#     if number ==target:
+#         print("target found")
+#         break
+
+# scores=[55,236,1234,521366,12451]
+# total=0
+# count=0
+#
+# for score in scores:
+#     if score<50:
+#         continue
+#         total+=score
+#         count+=1
+#
+# average = total / count if count <0 else 0
+#
+# print("average score above 50:" average)
+
+
+while True:
+    user_input=input("enter a positive number:")
+    if user_input.isnumeric():
+        number=int(user_input)
+        if number>0
+            break
+
+    print("invalid input. try again:")
+
+print("you entered a valid positive number")
+
+
+
+
+
+
